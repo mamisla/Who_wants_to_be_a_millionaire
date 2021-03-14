78620e7f4783b459f3dc003d7d77ac2af03685dd
mamisla <48299179+mamisla@users.noreply.github.com>
2021-03-14 23:41:38 +0100

    50 popravljen

--- Who_wants_to_be_a_millionaire/Form1.Designer.cs
+++ Who_wants_to_be_a_millionaire/Form1.Designer.cs
@@ -63,10 +63,9 @@ namespace Who_wants_to_be_a_millionaire
             this.a.BackColor = System.Drawing.SystemColors.MenuText;
             this.a.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.a.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.a.Location = new System.Drawing.Point(172, 412);
-            this.a.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.a.Location = new System.Drawing.Point(129, 335);
             this.a.Name = "a";
-            this.a.Size = new System.Drawing.Size(310, 46);
+            this.a.Size = new System.Drawing.Size(232, 37);
             this.a.TabIndex = 0;
             this.a.Text = "label1";
             this.a.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -77,11 +76,10 @@ namespace Who_wants_to_be_a_millionaire
             this.b.BackColor = System.Drawing.SystemColors.MenuText;
             this.b.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.b.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.b.Location = new System.Drawing.Point(609, 412);
-            this.b.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.b.Location = new System.Drawing.Point(457, 335);
             this.b.Name = "b";
-            this.b.Padding = new System.Windows.Forms.Padding(5);
-            this.b.Size = new System.Drawing.Size(301, 46);
+            this.b.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.b.Size = new System.Drawing.Size(226, 37);
             this.b.TabIndex = 1;
             this.b.Text = "label2";
             this.b.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -92,10 +90,9 @@ namespace Who_wants_to_be_a_millionaire
             this.c.BackColor = System.Drawing.SystemColors.MenuText;
             this.c.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.c.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.c.Location = new System.Drawing.Point(172, 473);
-            this.c.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.c.Location = new System.Drawing.Point(129, 384);
             this.c.Name = "c";
-            this.c.Size = new System.Drawing.Size(310, 45);
+            this.c.Size = new System.Drawing.Size(232, 37);
             this.c.TabIndex = 2;
             this.c.Text = "label3";
             this.c.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -106,10 +103,9 @@ namespace Who_wants_to_be_a_millionaire
             this.d.BackColor = System.Drawing.SystemColors.MenuText;
             this.d.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.d.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.d.Location = new System.Drawing.Point(618, 473);
-            this.d.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.d.Location = new System.Drawing.Point(464, 384);
             this.d.Name = "d";
-            this.d.Size = new System.Drawing.Size(301, 45);
+            this.d.Size = new System.Drawing.Size(226, 37);
             this.d.TabIndex = 3;
             this.d.Text = "label4";
             this.d.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -120,10 +116,9 @@ namespace Who_wants_to_be_a_millionaire
             this.pit.BackColor = System.Drawing.SystemColors.MenuText;
             this.pit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.pit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pit.Location = new System.Drawing.Point(197, 318);
-            this.pit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.pit.Location = new System.Drawing.Point(148, 258);
             this.pit.Name = "pit";
-            this.pit.Size = new System.Drawing.Size(643, 62);
+            this.pit.Size = new System.Drawing.Size(482, 50);
             this.pit.TabIndex = 4;
             this.pit.Text = "label5";
             this.pit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -133,10 +128,9 @@ namespace Who_wants_to_be_a_millionaire
             // 
             this.joker50.BackColor = System.Drawing.Color.Transparent;
             this.joker50.Image = ((System.Drawing.Image)(resources.GetObject("joker50.Image")));
-            this.joker50.Location = new System.Drawing.Point(4, 15);
-            this.joker50.Margin = new System.Windows.Forms.Padding(4);
+            this.joker50.Location = new System.Drawing.Point(3, 12);
             this.joker50.Name = "joker50";
-            this.joker50.Size = new System.Drawing.Size(89, 65);
+            this.joker50.Size = new System.Drawing.Size(67, 53);
             this.joker50.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.joker50.TabIndex = 5;
             this.joker50.TabStop = false;
@@ -146,10 +140,9 @@ namespace Who_wants_to_be_a_millionaire
             // 
             this.joker_zovi.BackColor = System.Drawing.Color.Transparent;
             this.joker_zovi.Image = ((System.Drawing.Image)(resources.GetObject("joker_zovi.Image")));
-            this.joker_zovi.Location = new System.Drawing.Point(115, 15);
-            this.joker_zovi.Margin = new System.Windows.Forms.Padding(4);
+            this.joker_zovi.Location = new System.Drawing.Point(86, 12);
             this.joker_zovi.Name = "joker_zovi";
-            this.joker_zovi.Size = new System.Drawing.Size(89, 65);
+            this.joker_zovi.Size = new System.Drawing.Size(67, 53);
             this.joker_zovi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.joker_zovi.TabIndex = 6;
             this.joker_zovi.TabStop = false;
@@ -159,10 +152,9 @@ namespace Who_wants_to_be_a_millionaire
             // 
             this.joker_publika.BackColor = System.Drawing.Color.Transparent;
             this.joker_publika.Image = ((System.Drawing.Image)(resources.GetObject("joker_publika.Image")));
-            this.joker_publika.Location = new System.Drawing.Point(221, 15);
-            this.joker_publika.Margin = new System.Windows.Forms.Padding(4);
+            this.joker_publika.Location = new System.Drawing.Point(166, 12);
             this.joker_publika.Name = "joker_publika";
-            this.joker_publika.Size = new System.Drawing.Size(89, 65);
+            this.joker_publika.Size = new System.Drawing.Size(67, 53);
             this.joker_publika.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.joker_publika.TabIndex = 7;
             this.joker_publika.TabStop = false;
@@ -173,10 +165,9 @@ namespace Who_wants_to_be_a_millionaire
             this.potrosen_joker_50.BackColor = System.Drawing.Color.Transparent;
             this.potrosen_joker_50.Cursor = System.Windows.Forms.Cursors.Default;
             this.potrosen_joker_50.Image = ((System.Drawing.Image)(resources.GetObject("potrosen_joker_50.Image")));
-            this.potrosen_joker_50.Location = new System.Drawing.Point(4, 15);
-            this.potrosen_joker_50.Margin = new System.Windows.Forms.Padding(4);
+            this.potrosen_joker_50.Location = new System.Drawing.Point(3, 12);
             this.potrosen_joker_50.Name = "potrosen_joker_50";
-            this.potrosen_joker_50.Size = new System.Drawing.Size(89, 65);
+            this.potrosen_joker_50.Size = new System.Drawing.Size(67, 53);
             this.potrosen_joker_50.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.potrosen_joker_50.TabIndex = 8;
             this.potrosen_joker_50.TabStop = false;
@@ -187,10 +178,9 @@ namespace Who_wants_to_be_a_millionaire
             this.potrosen_joker_zovi.BackColor = System.Drawing.Color.Transparent;
             this.potrosen_joker_zovi.Cursor = System.Windows.Forms.Cursors.Default;
             this.potrosen_joker_zovi.Image = ((System.Drawing.Image)(resources.GetObject("potrosen_joker_zovi.Image")));
-            this.potrosen_joker_zovi.Location = new System.Drawing.Point(115, 15);
-            this.potrosen_joker_zovi.Margin = new System.Windows.Forms.Padding(4);
+            this.potrosen_joker_zovi.Location = new System.Drawing.Point(86, 12);
             this.potrosen_joker_zovi.Name = "potrosen_joker_zovi";
-            this.potrosen_joker_zovi.Size = new System.Drawing.Size(89, 65);
+            this.potrosen_joker_zovi.Size = new System.Drawing.Size(67, 53);
             this.potrosen_joker_zovi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.potrosen_joker_zovi.TabIndex = 9;
             this.potrosen_joker_zovi.TabStop = false;
@@ -201,10 +191,9 @@ namespace Who_wants_to_be_a_millionaire
             this.potrosen_joker_publika.BackColor = System.Drawing.Color.Transparent;
             this.potrosen_joker_publika.Cursor = System.Windows.Forms.Cursors.Default;
             this.potrosen_joker_publika.Image = ((System.Drawing.Image)(resources.GetObject("potrosen_joker_publika.Image")));
-            this.potrosen_joker_publika.Location = new System.Drawing.Point(221, 15);
-            this.potrosen_joker_publika.Margin = new System.Windows.Forms.Padding(4);
+            this.potrosen_joker_publika.Location = new System.Drawing.Point(166, 12);
             this.potrosen_joker_publika.Name = "potrosen_joker_publika";
-            this.potrosen_joker_publika.Size = new System.Drawing.Size(89, 65);
+            this.potrosen_joker_publika.Size = new System.Drawing.Size(67, 53);
             this.potrosen_joker_publika.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.potrosen_joker_publika.TabIndex = 10;
             this.potrosen_joker_publika.TabStop = false;
@@ -213,10 +202,9 @@ namespace Who_wants_to_be_a_millionaire
             // redni
             // 
             this.redni.AutoSize = true;
-            this.redni.Location = new System.Drawing.Point(59, 43);
-            this.redni.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.redni.Location = new System.Drawing.Point(44, 35);
             this.redni.Name = "redni";
-            this.redni.Size = new System.Drawing.Size(46, 17);
+            this.redni.Size = new System.Drawing.Size(35, 13);
             this.redni.TabIndex = 11;
             this.redni.Text = "label1";
             this.redni.Visible = false;
@@ -225,6 +213,7 @@ namespace Who_wants_to_be_a_millionaire
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
@@ -241,24 +230,26 @@ namespace Who_wants_to_be_a_millionaire
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.MenuText;
-            this.splitContainer1.Panel2.Controls.Add(this.panela_txt);
-            this.splitContainer1.Panel2.Controls.Add(this.potrosen_joker_publika);
-            this.splitContainer1.Panel2.Controls.Add(this.joker50);
             this.splitContainer1.Panel2.Controls.Add(this.potrosen_joker_50);
+            this.splitContainer1.Panel2.Controls.Add(this.joker50);
+            this.splitContainer1.Panel2.Controls.Add(this.potrosen_joker_publika);
+            this.splitContainer1.Panel2.Controls.Add(this.panela_txt);
             this.splitContainer1.Panel2.Controls.Add(this.potrosen_joker_zovi);
             this.splitContainer1.Panel2.Controls.Add(this.joker_zovi);
             this.splitContainer1.Panel2.Controls.Add(this.joker_publika);
-            this.splitContainer1.Size = new System.Drawing.Size(1392, 573);
-            this.splitContainer1.SplitterDistance = 1048;
+            this.splitContainer1.Size = new System.Drawing.Size(1044, 466);
+            this.splitContainer1.SplitterDistance = 786;
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 12;
             // 
             // panela_txt
             // 
             this.panela_txt.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.panela_txt.Controls.Add(this.flowLayoutPanel1);
-            this.panela_txt.Location = new System.Drawing.Point(4, 98);
+            this.panela_txt.Location = new System.Drawing.Point(3, 80);
+            this.panela_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panela_txt.Name = "panela_txt";
-            this.panela_txt.Size = new System.Drawing.Size(324, 472);
+            this.panela_txt.Size = new System.Drawing.Size(243, 384);
             this.panela_txt.TabIndex = 11;
             this.panela_txt.Paint += new System.Windows.Forms.PaintEventHandler(this.panela_txt_Paint);
             // 
@@ -267,23 +258,23 @@ namespace Who_wants_to_be_a_millionaire
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.MenuText;
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(324, 472);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(243, 384);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Who_wants_to_be_a_millionaire.Properties.Resources.cila;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1392, 573);
+            this.ClientSize = new System.Drawing.Size(1044, 466);
             this.Controls.Add(this.redni);
             this.Controls.Add(this.splitContainer1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Tko želi biti milijunaš";
             this.Load += new System.EventHandler(this.Form1_Load);
